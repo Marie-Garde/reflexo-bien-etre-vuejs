440a834b38f6ff34d9b4f6adf43f02f963cb5e3c
Marie-Garde <marie_garde@ymail.com>
2025-09-05 12:11:59 +0200

    responsive tablet practices ok

---
+++ src/practices/components/practices/Benefits.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import AccordionItem from "@/components/AccordionItem.vue";
+import { useI18n } from "vue-i18n";
+
+const { t, tm } = useI18n();
+
+const benefitsList = tm("practices.benefits.list");
+
+defineProps<{
+  benefitsImageSource: string;
+}>();
+</script>
+
+<template>
+  <div class="benefits">
+    <h2>{{ t("practices.benefits.title") }}</h2>
+    <div class="benefits-content">
+      <img :src="benefitsImageSource" class="benefits-image" />
+      <ul class="benefits-list">
+        <li v-for="benefit in benefitsList">
+          <AccordionItem :title="benefit.title">
+            <div v-html="benefit.text"></div>
+          </AccordionItem>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/variables.scss" as *;
+
+.benefits {
+  width: 100%;
+  background-color: $green;
+  padding: 60px 0;
+  margin: 0 auto;
+  text-align: center;
+  width: 100%;
+  &-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 40px;
+    max-width: 1280px;
+    margin: 40px auto 0 auto;
+  }
+  &-list {
+    list-style: none;
+    padding: 0;
+    width: 70%;
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 768px) {
+  .benefits {
+    h2 {
+      padding: 0 10px;
+    }
+    &-list {
+      width: 90%;
+    }
+    &-content {
+      flex-direction: column-reverse;
+      gap: 20px;
+    }
+    &-image {
+      width: 90%;
+      max-width: 300px;
+    }
+  }
+}
+
+@media (min-width: 769px) and (max-width: 1200px) {
+  .benefits-content {
+    padding: 0 20px;
+  }
+}
+</style>
---
+++ src/practices/components/practices/Header.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const { t, tm } = useI18n();
+</script>
+
+<template>
+  <div class="header">
+    <div class="header-overlay">
+      <h1 class="title">{{ t("practices.title") }}</h1>
+      <h2 class="subTitle">{{ t("practices.presentation.title") }}</h2>
+      <p>{{ tm("practices.presentation.text")[0] }}</p>
+      <p>{{ tm("practices.presentation.text")[1] }}</p>
+      <p>{{ tm("practices.presentation.text")[2] }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/variables.scss" as *;
+
+.header {
+  width: 100%;
+  height: 60vh;
+  background-image: url("@/assets/background/practiceBackground.png");
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  display: flex;
+  align-items: center;
+  &-overlay {
+    margin: 0 auto;
+    max-width: 1280px;
+    width: 100%;
+    color: $white;
+    text-align: left;
+  }
+
+  .title {
+    margin-bottom: 50px;
+  }
+
+  .subTitle {
+    margin-bottom: 40px;
+  }
+
+  p {
+    max-width: 50%;
+  }
+}
+
+@media (max-width: 768px) {
+  .header {
+    height: auto;
+    background-image: none;
+    display: block;
+
+    &::before {
+      content: "";
+      display: block;
+      width: 100%;
+      height: 250px;
+      background-image: url("@/assets/background/practiceBackgroundMobile.png");
+      background-size: cover;
+      background-position: center;
+    }
+
+    &-container {
+      align-items: center;
+      justify-content: center;
+    }
+    &-overlay {
+      width: 90%;
+      margin-bottom: 0;
+      padding: 2rem 0;
+      text-align: center;
+      background: transparent;
+      box-shadow: none;
+      color: $brown-dark;
+    }
+    p {
+      max-width: 100%;
+    }
+  }
+}
+
+@media (min-width: 769px) and (max-width: 1200px) {
+  .header-overlay {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+}
+</style>
