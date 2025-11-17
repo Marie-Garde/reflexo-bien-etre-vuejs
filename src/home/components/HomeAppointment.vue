7b8e4bf1b02cee43e6d8392c6546e5ed8b526b2c
Marie-Garde <marie_garde@ymail.com>
2025-11-17 15:26:07 +0100

    use font inside

--- src/home/components/HomeAppointment.vue
+++ src/home/components/HomeAppointment.vue
@@ -40,7 +40,7 @@ function goToAppointment() {
   width: 100%;
   min-height: 40vh;
   height: auto;
-  background-image: url("/illustrations/homeCTA.webp");
+  background-image: url("/illustrations/homeCTA.jpg");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
